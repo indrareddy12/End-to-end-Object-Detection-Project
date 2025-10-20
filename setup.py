e0fa4d699302953f4a5d56563b0114422c84d64e
indrareddy12 <indrasenareddybathini86@gmail.com>
2025-10-20 12:30:13 +0530

    setup and requirements.txt added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'indrasenareddy',
+    author_email= 'indrasenareddybathini86@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
